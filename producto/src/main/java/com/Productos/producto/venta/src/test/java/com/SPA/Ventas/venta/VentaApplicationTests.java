f93f4fee27b04df90ebea3dd6c5714b68827dc5f
Vicente Rodriguez <vi.rodrigueze@duocuc.cl>
2025-05-14 16:52:53 -0400

    Primer commit de microservicio venta
    Vicente

---
+++ producto/src/main/java/com/Productos/producto/venta/src/test/java/com/SPA/Ventas/venta/VentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SPA.Ventas.venta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
