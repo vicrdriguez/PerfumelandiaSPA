c7bea3d3ef7fdf157a5698ccb1ad2e508e6783b1
OmniCroxx <163886177+OmniCroxx@users.noreply.github.com>
2025-05-14 16:19:57 -0400

    Primer commit de microservicio "Productos"
    Carpeta inicial de SpringBoot, con carpetas Service, Model, Controller y Repository vacias

---
+++ producto/src/test/java/com/Productos/producto/ProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Productos.producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
