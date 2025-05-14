49ca1bc7e93b347456495c9072ea841c4abe6446
Kevin-Duoc <k.fuenzalida@duocuc.cl>
2025-05-14 16:56:34 -0400

    Primer coomit de microservicio despacho
    Kevin

---
+++ despacho/src/main/java/com/SPA/Despachos/despacho/DespachoApplication.java
@@ -0,0 +1,13 @@
+package com.SPA.Despachos.despacho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespachoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespachoApplication.class, args);
+	}
+
+}
