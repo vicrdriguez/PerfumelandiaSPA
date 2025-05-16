dff5565eb19f5ff22acfa66baf059fba0a59c4c6
OmniCroxx <camiloyt1996@gmail.com>
2025-05-15 22:28:05 -0400

    Modelo y Conexion
    Modelo y Conexion Oracle Cloud, se conecta bien.

---
+++ producto/src/main/java/com/Productos/producto/Model/Producto.java
@@ -0,0 +1,39 @@
+package com.Productos.producto.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Producto")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "producto_seq")
+    @SequenceGenerator(name = "producto_seq", sequenceName = "PRODUCTO_SEQ", allocationSize = 1)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "Nombre", length = 100)
+    private String nombre;
+
+    @Column(name = "Descripcion")
+    private String descripcion;
+
+    @Column(name = "Precio")
+    private Double precio;
+
+    @Column(name = "Categoria", length = 50)
+    private String categoria;
+
+    @Column(name = "Marca", length = 50) 
+    private String marca;
+
+    
+}
