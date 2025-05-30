8f21dcbb163a0a9e2dbab454237c9ce70d4b9f24
Vicente Rodriguez <vi.rodrigueze@duocuc.cl>
2025-05-30 11:05:38 -0400

    Commit API VENTA CORROMPIDO
    PARA SACAR IDEA DEL FORMATO FECHA

---
+++ PerfumelandiaSPA-rama-ventas(Corrompido/venta/src/test/java/com/perfumelandiaspa/venta/VentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfumelandiaspa.venta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
